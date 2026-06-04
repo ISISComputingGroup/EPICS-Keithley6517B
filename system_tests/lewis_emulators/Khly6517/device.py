b0f2a9055055f43a203491510f1a65362e1c9385
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2026-06-04 12:47:25 +0100

    ruff

--- system_tests/lewis_emulators/Khly6517/device.py
+++ system_tests/lewis_emulators/Khly6517/device.py
@@ -1,21 +1,23 @@
 import queue
 import random
 from collections import OrderedDict
+from typing import Callable
 
+from lewis.core.statemachine import State
 from lewis.devices import StateMachineDevice
 
 from .states import DefaultState
 
 
 class Measurement:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.range = 0
         self.values = queue.Queue()
 
 
 class SimulatedKhly6517(StateMachineDevice):
-    def _initialize_data(self):
+    def _initialize_data(self) -> None:
         self.latest_reading = 0
         self.idle = True
 
@@ -32,40 +34,40 @@ class SimulatedKhly6517(StateMachineDevice):
         # Mock data
         self.random_mode = True
 
-    def _get_state_handlers(self):
+    def _get_state_handlers(self) -> dict[str, State]:
         return {
             "default": DefaultState(),
         }
 
-    def _get_initial_state(self):
+    def _get_initial_state(self) -> str:
         return "default"
 
-    def _get_transition_handlers(self):
+    def _get_transition_handlers(self) -> dict[tuple[str, str], Callable[[], bool]]:
         return OrderedDict([])
 
     # This command does not trigger a new reading, only return last reading
-    def fetch(self):
+    def fetch(self) -> float:
         return self.latest_reading
 
-    def abort(self):
+    def abort(self) -> None:
         self.idle = True
         self.latest_reading = 0
 
-    def initiate(self):
+    def initiate(self) -> None:
         self.idle = False
         self.measurement()
 
-    def get_selected_ranged_value(self):
+    def get_selected_ranged_value(self) -> float:
         value = self.selected_data.values.get()
         if value > self.selected_data.range:
             # Parameter data out of range
             self.add_error(-222)
         return max(min(value, self.selected_data.range), 0)
 
-    def add_random_reading(self):
+    def add_random_reading(self) -> None:
         self.selected_data.values.put(random.random() * self.selected_data.range)
 
-    def measurement(self):
+    def measurement(self) -> None:
         if self.random_mode:
             self.add_random_reading()
         if not self.selected_data.values.empty():
@@ -73,14 +75,14 @@ class SimulatedKhly6517(StateMachineDevice):
         else:
             self.latest_reading = 0
 
-    def add_error(self, error):
+    def add_error(self, error: int) -> None:
         if len(self.error_queue) < 9:
             self.error_queue.append(error)
         elif len(self.error_queue) == 9:
             # Queue overflow
             self.error_queue.append(-350)
 
-    def get_error(self):
+    def get_error(self) -> int:
         if len(self.error_queue) > 0:
             error = self.error_queue[0]
             self.error_queue = self.error_queue[1:]
@@ -88,13 +90,13 @@ class SimulatedKhly6517(StateMachineDevice):
         else:
             return 0
 
-    def clear_error_queue(self):
+    def clear_error_queue(self) -> None:
         self.error_queue = []
 
-    def add_mock_errors(self):
+    def add_mock_errors(self) -> None:
         self.add_error(2)
         self.add_error(5)
         self.add_error(10)
 
-    def insert_mock_readings(self, readings, mode):
+    def insert_mock_readings(self, readings: list[float], mode: str) -> None:
         [self.mode_dict[mode.lower()].values.put(reading) for reading in readings]
